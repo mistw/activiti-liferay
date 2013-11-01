4a4df760a350128a1be00cc9913e8d55561ad865
wangf <wangf@e-u.cn>
2013-11-01 17:54:06 +0800

    给引擎添加了字体设置，企图解决流程图乱码问题，但无效。 添加了候选用户实现的原型例子

--- activiti-web/src/main/java/gds/activiti/sr/GetFirstApproveCandidateGroupsForAssetTagsDelegate.java
+++ activiti-web/src/main/java/gds/activiti/sr/GetFirstApproveCandidateGroupsForAssetTagsDelegate.java
@@ -6,6 +6,7 @@ import static gds.activiti.sr.constants.TagConstants.TAG_PROJRCT_CREATE_CONTENT;
 import static gds.activiti.sr.constants.TagConstants.TAG_TECH_TASK_CONTENT;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -22,6 +23,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.Team;
 import com.liferay.portal.service.TeamLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
 
 /**
  * 该类通常用在流程定义文件中，用来根据内容类型来选择候选用户组
@@ -52,6 +54,12 @@ public class GetFirstApproveCandidateGroupsForAssetTagsDelegate implements JavaD
 		// 设置流程变量，流程中的UserTask中使用该流程变量来安排审核人
 		_log.info("-----"+StringUtils.join(cadidateGroupsForTags,","));
 		execution.setVariable("firstCandidateGroupList",cadidateGroupsForTags);
+		
+		// 设置流程变量，流程中的UserTask中使用该流程变量来安排审核人
+		// 获取与标签相对应的审核候选组
+		Collection<String> cadidateUsersForTags = getCadidateUsersForTags(execution, assetTags);		
+		_log.info("--AAAAAAAAAAAAAAAAAAAAA---"+StringUtils.join(cadidateUsersForTags,","));
+		execution.setVariable("firstCandidateUserList",StringUtils.join(cadidateUsersForTags,","));		
 	}
 	
 	private Collection<String> getCadidateGroupsForTags(DelegateExecution execution, Collection<String> assetTags) throws Exception {
@@ -78,4 +86,31 @@ public class GetFirstApproveCandidateGroupsForAssetTagsDelegate implements JavaD
 		return candidateRoleList;
 	}
 
+	private Collection<String> getCadidateUsersForTags(DelegateExecution execution, Collection<String> assetTags) throws Exception {
+		
+		List<String> candidateUserList = new ArrayList<String>();
+		long companyId = GetterUtil.getLong((Serializable)execution.getVariable(WorkflowConstants.CONTEXT_COMPANY_ID));
+		long groupId = GetterUtil.getLong((Serializable)execution.getVariable(WorkflowConstants.CONTEXT_GROUP_ID));
+		List<Team> allTeams=TeamLocalServiceUtil.getGroupTeams(groupId);
+		for (Team team : allTeams) {
+			//UserLocalServiceUtil.get
+		}
+		
+		//如果是技术保障任务
+		if (assetTags.contains(TAG_TECH_TASK_CONTENT)) {
+			candidateUserList.add(""+UserLocalServiceUtil.getUserByScreenName(companyId, "shih").getUserId());
+			_log.info("添加了用户: " + "shih");
+			candidateUserList.add(""+UserLocalServiceUtil.getUserByScreenName(companyId, "wanghx").getUserId());
+			_log.info("添加了用户: " + "wanghx");			
+		}
+		//如果是项目立项
+		if (assetTags.contains(TAG_PROJRCT_CREATE_CONTENT)) {
+			candidateUserList.add(""+UserLocalServiceUtil.getUserByScreenName(companyId, "shih").getUserId());
+			_log.info("添加了用户: " + "shih");
+			candidateUserList.add(""+UserLocalServiceUtil.getUserByScreenName(companyId, "wanghx").getUserId());
+			_log.info("添加了用户: " + "wanghx");
+		}
+		
+		return candidateUserList;
+	}
 }
